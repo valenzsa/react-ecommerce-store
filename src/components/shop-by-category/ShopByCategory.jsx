3095e71ef080968659fabec655fc4f5afcc5f759
valenzsa <valenzsa418@gmail.com>
2023-10-16 20:58:22 -0400

    Updated shop by category component

--- src/components/shop-by-category/ShopByCategory.jsx
+++ src/components/shop-by-category/ShopByCategory.jsx
@@ -1,23 +1,5 @@
 import "./ShopByCategory.css";
-
-const getCategories = [
-  {
-    name: "electronics",
-    image: "/src/assets/electronics.png",
-  },
-  {
-    name: "jewelry",
-    image: "/src/assets/jewelry.png",
-  },
-  {
-    name: "men's clothing",
-    image: "/src/assets/men.png",
-  },
-  {
-    name: "women's clothing",
-    image: "/src/assets/women.png",
-  },
-];
+import categories from "../../categoriesData";
 
 const ShopByCategory = () => {
   return (
@@ -25,9 +7,9 @@ const ShopByCategory = () => {
       <div className="shop-by-category">
         <h2>Shop By Category</h2>
         <ul>
-          {getCategories.map((category, index) => {
+          {categories.map((category) => {
             return (
-              <li key={index}>
+              <li key={category.id}>
                 <a href="#" title={category.name}>
                   <img src={category.image} alt={category.name} />
                   {category.name}
