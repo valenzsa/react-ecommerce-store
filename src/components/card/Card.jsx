c27511ef7e9b3019354b84c495d5a0ed99e390c7
valenzsa <valenzsa418@gmail.com>
2023-10-20 22:38:26 -0400

    Updating card component to add wishlist button - still wip

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,12 @@
-import { FaStar, FaHeart } from "react-icons/fa";
+import { useState, useRef } from "react";
+import { FaStar, FaHeart, FaRegHeart } from "react-icons/fa";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import "../card/Card.css";
 
 const Card = ({ electronics }) => {
+  const [wishList, setWishList] = useState(false);
+  const wishListRef = useRef(null);
+
   const hasDecimal = (num) => {
     return num % 1 !== 0;
   };
@@ -16,17 +21,54 @@ const Card = ({ electronics }) => {
     return star;
   };
 
+  // const wishList = () => {
+  //   console.log("mouseover");
+  //   <AiFillHeart />;
+  //   // <AiOutlineHeart />
+  // };
+
+  const myWishList = (id) => {
+    console.log(id);
+    console.log(wishListRef);
+    console.log(wishListRef.current.classList.contains("wishlisted"));
+
+    let cardCollection =
+      wishListRef.current.parentElement.parentElement.parentElement.children;
+
+    console.log(typeof cardCollection);
+
+    for (let card of cardCollection) {
+      console.log("card.id is: " + card.id);
+      console.log("id is: " + id);
+      if (card.id == id) {
+        //card.style.backgroundColor = "#ffcc00";
+        card.classList.add("wishlisted");
+      }
+    }
+  };
+
   return (
     <div className="card-container">
       {electronics.map((electronic) => {
         return (
-          <div className="card" key={electronic.id}>
+          <div className="card" key={electronic.id} id={electronic.id}>
             <div className="card-thumbnail">
-              <FaHeart />
+              <div
+                className="wishlist"
+                ref={wishListRef}
+                // onMouseOver={() => setWishList(electronic.id)}
+                // onMouseOut={() => setWishList(electronic.id)}
+
+                onMouseEnter={() => myWishList(electronic.id)}
+                //onMouseOut={() => myWishList(electronic.id)}
+              >
+                {wishList ? <AiFillHeart /> : <AiOutlineHeart />}
+              </div>
+
               <img src={electronic.image} alt={electronic.title} />
             </div>
             <div className="card-content">
-              <h3>{electronic.title.substring(0, 50)}...</h3>
+              <h3>{electronic.title.substring(0, 40)}...</h3>
               <div className="rating">
                 {/* {Math.floor(electronic.rating.rate)} */}
                 {electronic.rating.rate}
