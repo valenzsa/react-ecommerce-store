7029fc06c489cdcf79622cff72bd29e24f57e585
Santino Valenzuela <sev@storis.com>
2023-10-31 11:50:56 -0400

    Create loading component

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,13 @@
+import "../loading/Loading.css";
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="loading">
+        <p>Loading...</p>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
