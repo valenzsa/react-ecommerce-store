f03bc18c6ac527ed9b4ad9fac441d0110d19e2c5
valenzsa <valenzsa418@gmail.com>
2023-11-01 22:40:42 -0400

    Update homepage's latest products to display loader

--- src/components/latest-products/LatestProducts.jsx
+++ src/components/latest-products/LatestProducts.jsx
@@ -1,16 +1,29 @@
 import "../latest-products/LatestProducts.css";
 import Card from "../card/Card";
+import Loading from "../loading/Loading";
 
-const LatestProducts = ({ products, isOverlay, setIsOverlay }) => {
+const LatestProducts = ({
+  products,
+  isOverlay,
+  setIsOverlay,
+  isLoading,
+  setIsLoading,
+}) => {
   return (
     <div className="latest-products-container">
       <div className="latest-products">
         <h2>Latest Products</h2>
-        <Card
-          products={products}
-          isOverlay={isOverlay}
-          setIsOverlay={setIsOverlay}
-        />
+
+        {isLoading ? (
+          // Display loading component while fetching data
+          <Loading />
+        ) : (
+          <Card
+            products={products}
+            isOverlay={isOverlay}
+            setIsOverlay={setIsOverlay}
+          />
+        )}
       </div>
     </div>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,21 +9,38 @@ import Overlay from "../../components/overlay/Overlay";
 const Home = () => {
   const [isOverlay, setIsOverlay] = useState(false);
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const fetchLatestProducts = async () => {
+    setIsLoading(true);
+
     try {
       const response = await fetch("https://fakestoreapi.com/products?limit=6");
+
+      if (!response.ok) {
+        setIsLoading(true);
+        setIsError(true);
+      }
+
       const data = await response.json();
       //console.log(data);
       setProducts(data);
     } catch (error) {
       console.log(error);
+      setIsError(true);
     }
+
+    setIsLoading(false);
   };
 
   useEffect(() => {
     fetchLatestProducts();
   }, []);
+
+  if (isError) {
+    return `There is something wrong.  Check the console log`;
+  }
 
   return (
     <>
@@ -34,6 +51,8 @@ const Home = () => {
         products={products}
         isOverlay={isOverlay}
         setIsOverlay={setIsOverlay}
+        isLoading={isLoading}
+        setIsLoading={setIsLoading}
       />
       <Footer />
       {isOverlay ? <Overlay /> : null}
