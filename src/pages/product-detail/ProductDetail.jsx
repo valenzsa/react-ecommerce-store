9704ad39939582db3395a7f5e797c38da353a8be
valenzsa <valenzsa418@gmail.com>
2023-12-05 22:30:01 -0500

    Worked on updating the app, card and product detail components

--- src/pages/product-detail/ProductDetail.jsx
+++ src/pages/product-detail/ProductDetail.jsx
@@ -11,13 +11,44 @@ const ProductDetail = () => {
         <Breadcrumb />
         <div className="product-details">
           <div className="gallery">
-            <img src="https://placehold.co/500x500" alt="" />
+            <div className="slider">
+              <img src="https://placehold.co/500x500" alt="" />
+              <ul>
+                <li>
+                  <img src="https://placehold.co/100x100" alt="" />
+                </li>
+                <li>
+                  <img src="https://placehold.co/100x100" alt="" />
+                </li>
+                <li>
+                  <img src="https://placehold.co/100x100" alt="" />
+                </li>
+                <li>
+                  <img src="https://placehold.co/100x100" alt="" />
+                </li>
+              </ul>
+            </div>
+            <div className="product-description">
+              <h3>Product Description</h3>
+              <p>Product desciptions goes here...</p>
+            </div>
+            <div className="recommended-products"></div>
+            <div className="customer-reviews"></div>
           </div>
           <div className="overview">
-            <div className="product-title"></div>
-            <div className="product-description"></div>
-            <div className="product-price"></div>
-            <div className="add-to-cart"></div>
+            <div className="product-title">
+              <h3>Product Title</h3>
+            </div>
+            <div className="product-price">
+              <p>$499.99</p>
+            </div>
+            <div className="rating"></div>
+            <div className="add-to-cart">
+              <button>Add To Cart</button>
+            </div>
+            <div className="wishlist">
+              <button>Wishlist</button>
+            </div>
           </div>
         </div>
       </div>
